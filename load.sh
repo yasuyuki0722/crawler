91a903b46dbe302b2d9833b254c8b9ae2b2615a5
vagrant <vagrant@localhost.localdomain>
2014-06-03 10:03:43 +0900

    functionを別ファイルに

--- load.sh
+++ load.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #mysql -u root --password= -e "select count(*) from calendar.actopi"
 mysql -u root --password= -e "load data local infile 'actopi_url.tsv' into table calendar.actopi(@date, @title, @link, @file) set date=@date, title=@title, link=@link, file=@file, created_at=NOW(), update_at=NOW();"
-rm actopi_url.tsv
+cat /dev/null > actopi_url.tsv
