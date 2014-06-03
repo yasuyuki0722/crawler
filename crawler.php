752603ceeb87e7c43a933143161a24c3db696980
vagrant <vagrant@localhost.localdomain>
2014-06-03 10:34:35 +0900

    修正

--- crawler.php
+++ crawler.php
@@ -3,28 +3,22 @@ require_once('function.php');
 date_default_timezone_set('Asia/Tokyo');
 echo date('Y/m/d/H/i/s').'actopi crawler start >> ';
 $url = 'http://aucfan.com/article/?paged=';
+
 $page_number=pageNumberGet();
 var_dump($page_number);
 //exit;
 
 
-//url_data.txtの最新のurlをとる
-//$last_url=getLastUrl();
-//var_dump($last_url);
-//更新分取得
+/更新分取得
 $url_array=getLink($url, $page_number);
 //inputUrlData($url_array);
 var_dump($url_array);
-
-//更新分をurl_data.txtに書き込む
-//inputUrlData($url_data);
 
 //更新文url先のhtmlを取得
 getHtml($url_array);
 
 //更新分の中から必要な要素を取得してtsvに
 getTopic($url_array);
-
 
 //DBに書き込む
 inputData($url_array);
